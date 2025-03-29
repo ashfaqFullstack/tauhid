25f480ac1930010369c409a6c1b355b745b0be74
ashfaqFullStack <ashfaqahmadfullstack@gmail.com>
2025-03-28 22:25:33 -0700

    route set to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,31 @@
-import Image from "next/image";
-import { ModeToggle } from "@/components/Buttons/ModeToggle";
+import FilterItem from '@/components/Dashboard/Calendar/FilterItem'
+import Sections from '@/components/Dashboard/Calendar/Sections'
+import StartNewSection from '@/components/Dashboard/Calendar/StartNewSection'
+import Table from '@/components/Dashboard/Calendar/Table'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <ModeToggle /> */}
+    <div>
+      <StartNewSection />
+      <div className='flex items-center flex-wrap gap-[8px] my-[30px]' >
+        <FilterItem dropdown={false} filter="Date & Time" />
+        <FilterItem dropdown={true} filter="Amount" />
+        <FilterItem dropdown={false} filter="Date & Time" />
+        <FilterItem dropdown={false} filter="Date & Time" />
+        <FilterItem dropdown={false} filter="Date & Time" />
+        <div className='text-[#0866FF] text-[14px] font-semibold cursor-pointer hover:text-[#0831ff] px-[8px]' >
+          Clear Filter
+        </div>
+      </div>
+      <div className='my-[30px] flex gap-[20px] overflow-x-auto' >
+        <Sections />
+      </div>
+      <div className='overflow-x-auto ' >
 
+        <Table />
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
